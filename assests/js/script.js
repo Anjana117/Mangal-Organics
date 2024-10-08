24ad755bbb121f847f19f62a48f4d665054e760a
unknown <anjanakumari322001@gmail.com>
2024-10-08 16:45:34 +0530

    second commit

--- assests/js/script.js
+++ assests/js/script.js
@@ -2,33 +2,23 @@
 
 
 const header = document.querySelector('.header')
- window.addEventListener('scroll', () => {
-    if (window.scrollY >= 100) {
-        header.classList.add('header-scroll')
-        }
-        else if (window.scrollY < 100)
-        {
-        header.classList.remove('header-scroll')
-        }
-        })
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 100) {
+    header.classList.add('header-scroll')
+  }
+  else if (window.scrollY < 100) {
+    header.classList.remove('header-scroll')
+  }
+});
 
 
 
 
-        const swiper = new Swiper('.slider-wrapper', {
-            
-            loop: true,
-          
-            // If we need pagination
-            pagination: {
-              el: '.swiper-pagination',
-            },
-          
-            // Navigation arrows
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            },
-          
-            
-          });
+
+const accordion = document.getElementsByClassName('contentBx');
+for(i=0; i<accordion.length;i++){
+  accordion[i].addEventListener('click',function()
+  {
+    this.classList.toggle('active')
+  })
+}
